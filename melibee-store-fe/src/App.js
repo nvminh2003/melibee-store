9768d52498165494e918e23f2e5d37966cd07af6
nvminh2003 <minhnv.works@gmail.com>
2025-06-22 18:12:09 +0700

    set up FE

---
+++ melibee-store-fe/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./routes/routes";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
